57a68785096c30341b41cdf5b57d1fd75a512a2e
Jonathan Hawkins <uceeawk@ucl.ac.uk>
2021-10-26 20:10:25 +0100

    Configure setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="apreshttp",
     packages=find_packages(),
     include_package_data=True,
+    version='0.0.1',
+    description='HTTP interface for ApRES Radar',
+    author='Jono Hawkins',
+    author_email='jonathan.hawkins.17@ucl.ac.uk',
+    url='http://jonohawkins.co.uk/apreshttp'
 )
